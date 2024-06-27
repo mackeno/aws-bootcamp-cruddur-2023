1caa9617a31ad83257e63317f3133c3a7c940c46
mackeno luong <58233703+mackeno@users.noreply.github.com>
2024-06-27 19:37:24 +0000

    hardcoding

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -87,12 +87,11 @@ rollbar._build_request_data = _build_request_data
 
 def init_rollbar(app):
   rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-  flask_env = os.getenv('FLASK_ENV')
   rollbar.init(
       # access token
-      rollbar_access_token,
+      '2b9091e4634046ed8dd2fce67dbc351c',
       # environment name
-      flask_env,
+      'production',
       # server root directory, makes tracebacks prettier
       root=os.path.dirname(os.path.realpath(__file__)),
       # flask already sets up logging
